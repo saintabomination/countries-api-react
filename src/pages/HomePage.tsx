0ae99dced39e7b324a37580468f2c0c5d670b261
saintabomination <saint.abomination@protonmail.com>
2022-10-04 16:09:49 +0200

    Fetching country data from a REST API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
+import { useState, useEffect } from "react";
+
 import DefaultLayout from "../layouts/DefaultLayout";
 import Card from "../components/elements/Card";
 
-const HomePage = (): JSX.Element =>
-  (
+const HomePage = (): JSX.Element => {
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
+  return (
     <DefaultLayout>
       <h1>Home</h1>
       <Card>Card</Card>
@@ -12,5 +20,6 @@ const HomePage = (): JSX.Element =>
       <Card>Card</Card>
     </DefaultLayout>
   );
+}
 
 export default HomePage;
