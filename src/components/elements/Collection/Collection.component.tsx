aaed22491a673fffdc0f437a9d72885d4fcc59f4
saintabomination <saint.abomination@protonmail.com>
2022-10-04 16:36:57 +0200

    Basic styling for the Collection element

--- src/components/elements/Collection/Collection.component.tsx
+++ src/components/elements/Collection/Collection.component.tsx
@@ -1,10 +1,12 @@
+import './Collection.styles.scss';
+
 type Props = {
   children: React.ReactNode;
 };
 
 const Collection = ({ children }: Props): JSX.Element =>
   (
-    <div>
+    <div className="collection">
       {children}
     </div>
   );
