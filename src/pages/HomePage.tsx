0b7d8b3c5f1e3080b8452c062664016b008c32f4
saintabomination <saint.abomination@protonmail.com>
2022-10-04 16:49:04 +0200

    Search functionality

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,13 @@ import DefaultLayout from '../layouts/DefaultLayout';
 import Collection from '../components/elements/Collection';
 import Card from '../components/elements/Card';
 
+import type { ChangeEvent } from 'react';
+
 import type { Country } from '../typings/countryTypes';
 
 const HomePage = (): JSX.Element => {
   const [countryData, setCountryData] = useState<Country[]>([]);
+  const [countryQuery, setCountryQuery] = useState<string>('');
 
   const fetchCountries = async (): Promise<Country[]> => {
     const res = await fetch('https://restcountries.com/v3.1/all');
@@ -19,13 +22,20 @@ const HomePage = (): JSX.Element => {
     fetchCountries().then(countries => setCountryData(countries));
   }, []);
 
-  console.log(countryData);
+  const filteredCountries = countryData.filter(
+    country =>
+    country.name.common.toLowerCase().includes(countryQuery.toLowerCase())
+  );
 
   return (
     <DefaultLayout>
       <h1>Home</h1>
+      <input
+        type="text"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setCountryQuery(e.target.value)} defaultValue={countryQuery}
+      />
       <Collection>
-        {countryData.map((country, index) => (
+        {filteredCountries && filteredCountries.map((country, index) => (
           <Card key={index}>
             <img src={country.flags.png} alt={country.name.common} height={48} />
             <p>{country.name.common}</p>
