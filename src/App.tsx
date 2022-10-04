726dc2c185bf71556b6818f9ec9e00c1574d8ed0
saintabomination <saint.abomination@protonmail.com>
2022-10-04 15:56:17 +0200

    Adding and setting up React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import PageNavigation from './components/blocks/PageNavigation';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
 
 const App = (): JSX.Element =>
   (
-    <div>
-      <PageNavigation />
-      <h1>Initial Commit</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import PageNavigation from '../components/blocks/PageNavigation';
+
+const HomePage = (): JSX.Element =>
+  (
+    <div>
+      <PageNavigation />
+      <h1>Home</h1>
+    </div>
+  );
+
+export default HomePage;
