2699b9adc3b650102e35e1466ede5b5afbde22c7
saintabomination <saint.abomination@protonmail.com>
2022-10-04 15:52:50 +0200

    Creating the Page Navigation block

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import PageNavigation from './components/blocks/PageNavigation';
+
 const App = (): JSX.Element =>
   (
     <div>
+      <PageNavigation />
       <h1>Initial Commit</h1>
     </div>
   );
---
+++ src/components/blocks/PageNavigation/PageNavigation.component.tsx
@@ -0,0 +1,13 @@
+const PageNavigation = (): JSX.Element =>
+  (
+    <nav>
+      <h2>Page Navigation</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </nav>
+  );
+
+export default PageNavigation;
